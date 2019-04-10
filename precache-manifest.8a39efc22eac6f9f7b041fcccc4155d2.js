910fc54a0b7e684fb127c25f84befc9ab5a8daab
marcel tumboimbela <jonathan.marcel.t@gmail.com>
2019-04-10 08:05:47 +0700

    Updates

---
+++ precache-manifest.8a39efc22eac6f9f7b041fcccc4155d2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "db812d8a70a4e88e888744c1c9a27e89",
+    "url": "/marcel/static/media/fontawesome-webfont.db812d8a.woff2"
+  },
+  {
+    "revision": "abaed7317ac52d4462d9",
+    "url": "/marcel/static/css/main.7d77a254.chunk.css"
+  },
+  {
+    "revision": "37d8927d407b71fc3afe",
+    "url": "/marcel/static/js/runtime~main.cc870198.js"
+  },
+  {
+    "revision": "cddfca11dc3d7caf22e7",
+    "url": "/marcel/static/js/2.242c46d0.chunk.js"
+  },
+  {
+    "revision": "8eebe3dce035b7178d707cf24a539233",
+    "url": "/marcel/static/media/003cad1.8eebe3dc.jpg"
+  },
+  {
+    "revision": "abaed7317ac52d4462d9",
+    "url": "/marcel/static/js/main.2817d037.chunk.js"
+  },
+  {
+    "revision": "32400f4e08932a94d8bfd2422702c446",
+    "url": "/marcel/static/media/fontawesome-webfont.32400f4e.eot"
+  },
+  {
+    "revision": "a35720c2fed2c7f043bc7e4ffb45e073",
+    "url": "/marcel/static/media/fontawesome-webfont.a35720c2.woff"
+  },
+  {
+    "revision": "a3de2170e4e9df77161ea5d3f31b2668",
+    "url": "/marcel/static/media/fontawesome-webfont.a3de2170.ttf"
+  },
+  {
+    "revision": "f775f9cca88e21d45bebe185b27c0e5b",
+    "url": "/marcel/static/media/fontawesome-webfont.f775f9cc.svg"
+  },
+  {
+    "revision": "c5974746e25dc76d1ae8399ca089f402",
+    "url": "/marcel/index.html"
+  }
+];
